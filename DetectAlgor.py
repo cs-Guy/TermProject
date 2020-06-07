223f8a51bd0677d02f9757ab0f9592662b3dc560
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2020-06-07 19:10:05 +0700

    Update

--- DetectAlgor.py
+++ DetectAlgor.py
@@ -1,117 +1,121 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May  7 23:50:16 2020
+Created on Thu May  7 21:23:23 2020
 
 @author: Guy
 """
 
-
 import cv2
 import numpy as np
 import math
+import time
+import pyautogui
 
 
-cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
-    # read image
-    ret, img = cap.read()
+pyautogui.FAILSAFE = False
+SCREEN_X, SCREEN_Y = pyautogui.size()
+CLICK = CLICK_MESSAGE = MOVEMENT_START = None
 
-    # get hand data from the rectangle sub window on the screen
+
+cam = cv2.VideoCapture(0)
+
+while cam.isOpened():
+    ret, img = cam.read()
+    
+    # cv2.rectangle(img, (300,300), (100,100), (0,255,0),0)
+    # crop_img = img[100:300, 100:300]
+    img = cv2.flip(img, 1)
+    # crop_img = img
     cv2.rectangle(img, (200,70), (550,400), (0,255,0),0)
     crop_img = img[70:400, 200:550]
-
-    # convert to grayscale
+    CAMERA_X, CAMERA_Y, channels = crop_img.shape
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-
-    # applying gaussian blur
     value = (35, 35)
     blurred = cv2.GaussianBlur(grey, value, 0)
-
-    # thresholdin: Otsu's Binarization method
-    _, thresh1 = cv2.threshold(blurred, 127, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-    # show thresholded image
+    cv2.imshow('Blured', blurred)
+    _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     cv2.imshow('Thresholded', thresh1)
-
-    # check OpenCV version to avoid unpacking error
-    (version, _, _) = cv2.__version__.split('.')
-
-    if version == '3':
-        image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
-               cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    elif version == '2':
-        contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
-               cv2.CHAIN_APPROX_NONE)
-
-    # find contour with max area
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
-
-    # create bounding rectangle around the contour (can skip below two lines)
+    
+    image, contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    max_area = -1
+    for i in range(len(contours)):
+        cnt = contours[i]
+        area = cv2.contourArea(cnt)
+        if area > max_area:
+            max_area = area
+            ci = i
+    cnt = contours[ci]
     x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
-
-    # finding convex hull
+    cv2.rectangle(crop_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
     hull = cv2.convexHull(cnt)
-
-    # drawing contours
-    drawing = np.zeros(crop_img.shape,np.uint8)
+    drawing = np.zeros(crop_img.shape, np.uint8)
     cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
-
-    # finding convex hull
+    cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 0)
     hull = cv2.convexHull(cnt, returnPoints=False)
-
-    # finding convexity defects
     defects = cv2.convexityDefects(cnt, hull)
     count_defects = 0
     cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
 
-    # applying Cosine Rule to find angle for all defects (between fingers)
-    # with angle > 90 degrees and ignore defects
+    used_defect = None
     for i in range(defects.shape[0]):
-        s,e,f,d = defects[i,0]
-
+        s, e, f, d = defects[i, 0]
         start = tuple(cnt[s][0])
         end = tuple(cnt[e][0])
         far = tuple(cnt[f][0])
-
-        # find length of all sides of triangle
-        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-
-        # apply cosine rule here
-        angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
-
-        # ignore angles > 90 and highlight rest with red dots
+        a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+        b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+        c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+        angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
         if angle <= 90:
             count_defects += 1
-            cv2.circle(crop_img, far, 1, [0,0,255], -1)
-        #dist = cv2.pointPolygonTest(cnt,far,True)
+        cv2.circle(crop_img, far, 5, [0, 0, 255], -1)
+        cv2.line(crop_img, start, end, [0, 255, 0], 2)
+        medium_x = (start[0] + end[0]) / 2
+        medium_y = (start[1] + end[1]) / 2
 
-        # draw a line from start to end i.e. the convex points (finger tips)
-        # (can skip this part)
-        cv2.line(crop_img,start, end, [0,255,0], 2)
-        #cv2.circle(crop_img,far,5,[0,0,255],-1)
-        
-    # define actions required
-    if count_defects == 1:
-        cv2.putText(img,"two finger", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    elif count_defects == 2:
-        str = "three"
-        cv2.putText(img, str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-    elif count_defects == 3:
-        cv2.putText(img,"four", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    elif count_defects == 4:
-        cv2.putText(img,"five", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        if count_defects == 1 and angle <= 90:
+            used_defect = {"x": start[0], "y": start[1]}
+
+    if used_defect is not None:
+        best = used_defect
+        if count_defects == 1:
+            x = best['x']
+            y = best['y']
+            display_x = x
+            display_y = y
+
+            if MOVEMENT_START is not None:
+                M_START = (x, y)
+                x = x - MOVEMENT_START[0]
+                y = y - MOVEMENT_START[1]
+                x = x * (SCREEN_X / CAMERA_X)
+                y = y * (SCREEN_Y / CAMERA_Y)
+                MOVEMENT_START = M_START
+                print("X: " + str(x) + " Y: " + str(y))
+                pyautogui.moveRel(x, y)
+            else:
+                MOVEMENT_START = (x, y)
+
+            cv2.circle(crop_img, (display_x, display_y), 5, [255, 255, 255], 20)
+        elif count_defects == 4 and CLICK is None:
+            CLICK = time.time()
+            pyautogui.click()
+            CLICK_MESSAGE = "LEFT CLICK"
+        elif count_defects == 3 and CLICK is None:
+            CLICK = time.time()
+            pyautogui.rightClick()
+            CLICK_MESSAGE = "RIGHT CLICK"
     else:
-        cv2.putText(img,"none", (50, 50),\
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        MOVEMENT_START = None
 
-    # show appropriate images in windows
+    if CLICK is not None:
+        cv2.putText(img, CLICK_MESSAGE, (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 3, 3)
+        if CLICK < time.time():
+             CLICK = None
+
+    cv2.putText(img, "Defects: " + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     cv2.imshow('Gesture', img)
-    all_img = np.hstack((drawing, crop_img))
-    cv2.imshow('Contours', all_img)
+    cv2.imshow('Drawing', drawing)
 
     k = cv2.waitKey(10)
     if k == 27:
